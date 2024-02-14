ecf35d20c78e657a7215214a8defdb2e861fa966
NatsuCamellia <natsucamellia@gmail.com>
2024-02-14 20:48:13 +0800

    Initial commit

---
+++ src/main/java/net/natsucamellia/fireworkextensions/FireworkExtensions.java
@@ -0,0 +1,16 @@
+package net.natsucamellia.fireworkextensions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FireworkExtensions implements ModInitializer {
+	public static final String MOD_ID = "fireworkextensions";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
