cec32bf93f8392a0143213ace005661c6041301f
NatsuCamellia <natsucamellia@gmail.com>
2024-02-17 21:04:31 +0800

    Add protected explodeBall method

--- src/main/java/net/natsucamellia/fireworkextensions/shape/ExplosionShape.java
+++ src/main/java/net/natsucamellia/fireworkextensions/shape/ExplosionShape.java
@@ -34,6 +34,23 @@ public abstract class ExplosionShape {
         }
     }
 
+    protected void explodeBall(double size, int amount, Vec3d coords, int[] colors, int[] fadeColors, boolean trail, boolean flicker, ParticleManager particleManager) {
+        for (int i = -amount; i <= amount; ++i) {
+            for (int j = -amount; j <= amount; ++j) {
+                for (int k = -amount; k <= amount; ++k) {
+                    double g = (double)j + (this.random.nextDouble() - this.random.nextDouble()) * 0.5;
+                    double h = (double)i + (this.random.nextDouble() - this.random.nextDouble()) * 0.5;
+                    double l = (double)k + (this.random.nextDouble() - this.random.nextDouble()) * 0.5;
+                    double m = Math.sqrt(g * g + h * h + l * l) / size + this.random.nextGaussian() * 0.05;
+                    Vec3d velocity = new Vec3d(g / m, h / m, l / m);
+                    this.addExplosionParticle(coords, velocity, colors, fadeColors, trail, flicker, particleManager);
+                    if (i == -amount || i == amount || j == -amount || j == amount) continue;
+                    k += amount * 2 - 1;
+                }
+            }
+        }
+    }
+
     public Identifier getIdentifier() {
         return identifier;
     }
