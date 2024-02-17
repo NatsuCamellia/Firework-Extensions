09487d6b5403b119aa0c9967bd754253db09925c
NatsuCamellia <natsucamellia@gmail.com>
2024-02-17 21:04:45 +0800

    Register vanilla LARGE_BALL

--- src/main/java/net/natsucamellia/fireworkextensions/shape/VanillaExplosionShapes.java
+++ src/main/java/net/natsucamellia/fireworkextensions/shape/VanillaExplosionShapes.java
@@ -15,8 +15,16 @@ public class VanillaExplosionShapes {
         }
     };
 
+    public static ExplosionShape LARGE_BALL = new ExplosionShape(new Identifier("small_ball"), List.of()) {
+        @Override
+        public void explode(Vec3d coords, int[] colors, int[] fadeColors, boolean trail, boolean flicker, ParticleManager particleManager) {
+            explodeBall(0.5, 4, coords, colors, fadeColors, trail, flicker, particleManager);
+        }
+    };
+
     public static void registerVanillaExplosionShapes() {
         FireworkExtensions.LOGGER.info("Registering vanilla explosion shapes");
         FireworkExtensions.registerExplosionShape(SMALL_BALL);
+        FireworkExtensions.registerExplosionShape(LARGE_BALL);
     }
 }
