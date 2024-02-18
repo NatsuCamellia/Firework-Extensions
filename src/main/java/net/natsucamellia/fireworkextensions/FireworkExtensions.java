e9226cda43e8b8c9edd96a0eea8ca0e904a88745
NatsuCamellia <natsucamellia@gmail.com>
2024-02-18 14:15:56 +0800

    Mixin for custom firework star ingredients

--- src/main/java/net/natsucamellia/fireworkextensions/FireworkExtensions.java
+++ src/main/java/net/natsucamellia/fireworkextensions/FireworkExtensions.java
@@ -2,11 +2,15 @@ package net.natsucamellia.fireworkextensions;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.recipe.Ingredient;
 import net.natsucamellia.fireworkextensions.shape.ExplosionShape;
 import net.natsucamellia.fireworkextensions.shape.VanillaExplosionShapes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -15,6 +19,8 @@ public class FireworkExtensions implements ModInitializer {
 	public static final String MOD_ID = "fireworkextensions";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	private static final Map<Integer, ExplosionShape> explosionShapes = new HashMap<>();
+	private static final Map<Item, Integer> ingredientShapeIdMap = new HashMap<>();
+	private static Ingredient TYPE_MODIFIER = null;
 	private static int shapeCount = 0;
 
 	@Override
@@ -26,9 +32,22 @@ public class FireworkExtensions implements ModInitializer {
 	public static ExplosionShape registerExplosionShape(ExplosionShape shape) {
 		LOGGER.info("Registering explosion type " + shape.getIdentifier());
 		if (!explosionShapes.containsValue(shape)) {
-			explosionShapes.put(shapeCount++, shape);
+			explosionShapes.put(shapeCount, shape);
+			shape.getIngredientItems().forEach(item -> ingredientShapeIdMap.put(item, shapeCount));
+			shapeCount++;
 		}
 		return shape;
+	}
+
+	public static Ingredient getTypeModifier() {
+		if (TYPE_MODIFIER == null) {
+			TYPE_MODIFIER = Ingredient.ofItems(explosionShapes.values().stream().map(ExplosionShape::getIngredientItems).flatMap(Collection::stream).toArray(ItemConvertible[]::new));
+		}
+		return TYPE_MODIFIER;
+	}
+
+	public static Integer getExplosionShapeIdByIngredientItem(Item item) {
+		return ingredientShapeIdMap.get(item);
 	}
 
 	public static Optional<ExplosionShape> getExplosionShapeById(int id) {
---
+++ src/main/java/net/natsucamellia/fireworkextensions/mixin/FireworkStarRecipeMixin.java
@@ -0,0 +1,108 @@
+package net.natsucamellia.fireworkextensions.mixin;
+
+import com.google.common.collect.Lists;
+import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.item.DyeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.FireworkStarRecipe;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.world.World;
+import net.natsucamellia.fireworkextensions.FireworkExtensions;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+
+@Mixin(FireworkStarRecipe.class)
+public class FireworkStarRecipeMixin {
+    @Final
+    @Shadow
+    private static Ingredient FLICKER_MODIFIER;
+
+    @Final
+    @Shadow
+    private static Ingredient TRAIL_MODIFIER;
+
+    @Final
+    @Shadow
+    private static Ingredient GUNPOWDER;
+
+    @Inject(method = "matches", at = @At("HEAD"), cancellable = true)
+    void matches(RecipeInputInventory recipeInputInventory, World world, CallbackInfoReturnable<Boolean> ci) {
+        boolean gunpowder = false;
+        boolean dye = false;
+        boolean type = false;
+        boolean trail = false;
+        boolean flicker = false;
+        for (int i = 0; i < recipeInputInventory.size(); ++i) {
+            ItemStack itemStack = recipeInputInventory.getStack(i);
+            if (itemStack.isEmpty()) continue;
+
+            if (FireworkExtensions.getTypeModifier().test(itemStack)) {
+                if (type) {
+                    ci.setReturnValue(false);
+                    return;
+                }
+                type = true;
+            } else if (FLICKER_MODIFIER.test(itemStack)) {
+                if (flicker) {
+                    ci.setReturnValue(false);
+                    return;
+                }
+                flicker = true;
+            } else if (TRAIL_MODIFIER.test(itemStack)) {
+                if (trail) {
+                    ci.setReturnValue(false);
+                    return;
+                }
+                trail = true;
+            } else if (GUNPOWDER.test(itemStack)) {
+                if (gunpowder) {
+                    ci.setReturnValue(false);
+                    return;
+                }
+                gunpowder = true;
+            } else if (itemStack.getItem() instanceof DyeItem) {
+                dye = true;
+            } else {
+                ci.setReturnValue(false);
+                return;
+            }
+        }
+        ci.setReturnValue(gunpowder && dye);
+    }
+
+    @Inject(method = "craft", at = @At("HEAD"), cancellable = true)
+    void craft(RecipeInputInventory recipeInputInventory, DynamicRegistryManager dynamicRegistryManager, CallbackInfoReturnable<ItemStack> ci) {
+        ItemStack itemStack = new ItemStack(Items.FIREWORK_STAR);
+        NbtCompound nbtCompound = itemStack.getOrCreateSubNbt("Explosion");
+        int typeId = 0;
+        ArrayList<Integer> colorList = Lists.newArrayList();
+
+        for (int i = 0; i < recipeInputInventory.size(); ++i) {
+            ItemStack itemStack2 = recipeInputInventory.getStack(i);
+            if (itemStack2.isEmpty()) continue;
+
+            if (FireworkExtensions.getTypeModifier().test(itemStack2)) {
+                typeId = FireworkExtensions.getExplosionShapeIdByIngredientItem(itemStack2.getItem());
+            } else if (FLICKER_MODIFIER.test(itemStack2)) {
+                nbtCompound.putBoolean("Flicker", true);
+            } else if (TRAIL_MODIFIER.test(itemStack2)) {
+                nbtCompound.putBoolean("Trail", true);
+            } else if (itemStack2.getItem() instanceof DyeItem) {
+                colorList.add(((DyeItem)itemStack2.getItem()).getColor().getFireworkColor());
+            }
+        }
+
+        nbtCompound.putIntArray("Colors", colorList);
+        nbtCompound.putByte("Type", (byte)typeId);
+        ci.setReturnValue(itemStack);
+    }
+}
