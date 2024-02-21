884e89d6058f0ab68db90173a1e323ba1d1649a7
NatsuCamellia <natsucamellia@gmail.com>
2024-02-21 22:12:50 +0800

    Add explodeStar

--- src/main/java/net/natsucamellia/fireworkextensions/shape/ExplosionShape.java
+++ src/main/java/net/natsucamellia/fireworkextensions/shape/ExplosionShape.java
@@ -6,6 +6,7 @@ import net.minecraft.item.Item;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.random.Random;
 import net.natsucamellia.fireworkextensions.mixin.ParticleAccessor;
@@ -61,6 +62,44 @@ public abstract class ExplosionShape {
                     if (i == -amount || i == amount || j == -amount || j == amount) continue;
                     k += amount * 2 - 1;
                 }
+            }
+        }
+    }
+
+    /**
+     * Explodes in mirrored {@code pattern} shape for {@code planes} times, rotating the Y-axis.
+     * @param size the size of the shape
+     * @param pattern a symmetric pattern of the explosion, only provide one side, the other side is mirrored automatically.
+     * @param planes the number of planes
+     * @param keepShape {@code true} for no rotating. (10% rotation compared to {@code false})
+     */
+    protected void explodeStar(double size, double[][] pattern, int planes, Vec3d coords, int[] colors, int[] fadeColors, boolean trail, boolean flicker, boolean keepShape, ParticleManager particleManager) {
+        double startH = pattern[0][0]; // Horizontal coordinate
+        double startV = pattern[0][1]; // Vertical coordinate
+        // Starting point
+        this.addExplosionParticle(coords, new Vec3d(startH * size, startV * size, 0.0), colors, fadeColors, trail, flicker, particleManager);
+        float thetaOffset = this.random.nextFloat() * (float)Math.PI; // Give random horizontal facing
+        double rotationFactor = keepShape ? 0.1 / planes : 1.0 / planes;
+        for (int i = 0; i < planes; ++i) {
+            double theta = (double)thetaOffset + (double)((float)i * (float)Math.PI) * rotationFactor;
+            double lastH = startH;
+            double lastV = startV;
+            for (int l = 1; l < pattern.length; ++l) {
+                double currH = pattern[l][0];
+                double currV = pattern[l][1];
+                // Linear interpolation
+                for (double o = 0.25; o <= 1.0; o += 0.25) {
+                    double x = MathHelper.lerp(o, lastH, currH) * size;
+                    double y = MathHelper.lerp(o, lastV, currV) * size;
+                    double z = x * Math.sin(theta);
+                    x *= Math.cos(theta);
+                    // Mirror
+                    for (double s = -1.0; s <= 1.0; s += 2.0) {
+                        this.addExplosionParticle(coords, new Vec3d(x * s, y, z * s), colors, fadeColors, trail, flicker, particleManager);
+                    }
+                }
+                lastH = currH;
+                lastV = currV;
             }
         }
     }
